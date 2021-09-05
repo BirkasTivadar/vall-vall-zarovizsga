748de767b48f4062b9a3a4f767da33022817fa02
BirkasTivadar <birkastivadar@gmail.com>
2021-09-05 17:21:39 +0200

    Tesztek átmásolva

--- src/main/java/org/training360/finalexam/Sv2021JvjbfZarovizsgaApplication.java
+++ src/main/java/org/training360/finalexam/Sv2021JvjbfZarovizsgaApplication.java
@@ -1,13 +1,19 @@
 package org.training360.finalexam;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Sv2021JvjbfZarovizsgaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Sv2021JvjbfZarovizsgaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Sv2021JvjbfZarovizsgaApplication.class, args);
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
